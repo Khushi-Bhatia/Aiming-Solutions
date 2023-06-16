9bd6051e72be59fe61bce1be69d342090b07bd26
Khushi-Bhatia <khushibhatiaabc@gmail.com>
2023-06-16 20:25:42 +0530

    added all files

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// const boxes = document.querySelectorAll('.box');
+
+// boxes.forEach(box => {
+//   box.addEventListener('mouseover', () => {
+//     box.style.transform = 'scale(1.1)';
+//   });
+
+//   box.addEventListener('mouseout', () => {
+//     box.style.transform = 'scale(1)';
+//   });
+// });
